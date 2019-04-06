d0231665d9f628c14d1266b85ff423b38c8b81ed
Andreas Diehl <adiehl.developer@bitquadrat.de>
2019-04-07 00:01:17 +0200

    Facebook account activity filled with zeroes between

--- src/app/facebook/facebook.component.ts
+++ src/app/facebook/facebook.component.ts
@@ -17,9 +17,24 @@ export class FacebookComponent implements OnInit {
   }
 
   public async loadDataAndShowChart() {
-    const stats = await this.getStats();
-    const dates = [];
-    const numbers = [];
+    const statsObj = await this.getStats();
+    const stats = statsObj.activities;
+    const currentDate = statsObj.firstDate;
+    console.log('DATES:');
+    console.log(currentDate);
+    console.log(statsObj.lastDate);
+    while (currentDate > statsObj.lastDate) {
+      const shortDate = this.getDateShort(currentDate);
+      let count = 0;
+      if (stats[shortDate]) {
+        count = stats[shortDate];
+      }
+      this.numbers.push(count);
+
+      this.labels.push(shortDate);
+      currentDate.setDate(currentDate.getDate() - 1);
+      console.log(`Date: ${currentDate} Count: ${count}`);
+    }
     for (const stat of Object.keys(stats)) {
       this.numbers.push(stats[stat]);
       this.labels.push(stat);
@@ -31,18 +46,28 @@ export class FacebookComponent implements OnInit {
     const data = await this.db.getFile('facebook_activity');
     const structuredData = JSON.parse(data.toString());
     const activities = {};
+    let firstDate;
+    let lastDate;
     for (const entry of structuredData.account_activity) {
       if (!entry.timestamp) {
         continue;
       }
       const myDate = new Date(entry.timestamp * 1000);
+      if (!firstDate) {
+        firstDate = myDate;
+      }
       if (activities[this.getDateShort(myDate)]) {
         activities[this.getDateShort(myDate)]++;
       } else {
         activities[this.getDateShort(myDate)] = 1;
       }
+      lastDate = myDate;
     }
-    return activities;
+    return {
+      activities,
+      firstDate,
+      lastDate
+    };
   }
 
   private getDateShort(date: Date) {
@@ -51,7 +76,7 @@ export class FacebookComponent implements OnInit {
         ('00' + (date.getUTCMonth() + 1)).slice(-2) + '-' +
         ('00' + date.getUTCDate()).slice(-2);
 
-    console.log(newDate);
+    //console.log(newDate);
     return newDate;
   }
   ngOnInit() {
