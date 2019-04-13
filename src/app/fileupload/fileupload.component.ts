94f760e0c94c20cbcbbbe4a79aa5b5f6d5346d2b
Andreas Diehl <adiehl.developer@bitquadrat.de>
2019-04-13 16:39:56 +0200

    Show what has been uploaded already by indicating a check-mark

--- src/app/fileupload/fileupload.component.ts
+++ src/app/fileupload/fileupload.component.ts
@@ -9,9 +9,22 @@ import { DatabaseService } from '../services/database.service';
 
 export class FileuploadComponent {
   public dataTypes = [];
+  public uploads = {};
   public fileType = 'amazon_searches';
   constructor(public db: DatabaseService) {
     this.dataTypes = db.dataTypes;
+    this.checkDataTypes();
+  }
+
+  checkDataTypes() {
+    for (const dataType of this.dataTypes) {
+      this.uploads[dataType.name] = false;
+      this.db.getFile(dataType.name).then((data) => {
+        if (data) {
+          this.uploads[dataType.name] = true;
+        }
+      });
+    }
   }
   handleFileInput(files) {
     const reader = new FileReader();
@@ -21,6 +34,7 @@ export class FileuploadComponent {
       const save = reader.result;
       console.log(save); // {hp: 32, maxHp: 50, mp: 11, maxMp: 23}
       this.db.saveFile(this.fileType, save.toString());
+      this.checkDataTypes();
     };
   }
   deleteData() {
